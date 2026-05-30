57a8f6f2d832d1fb2c635edabd02abb76c819ef3
mikeydebug <cs24b1031@iiitr.ac.in>
2026-05-30 14:18:37 +0530

    fix: replace deprecated express wildcard route for Render compatibility

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const path = require('path');
 const frontendPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(frontendPath));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
